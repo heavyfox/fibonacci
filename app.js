5e6919dfc0fbcb84edcab7874837cf8c1b4ef142
heavyfox <steamtrader@gmai.com>
2020-09-08 20:08:24 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-    
-    if (n === 0) {
-        return 0; 
-    } else if (n === 1) {
-        return 1;
+    //メモにデータが有ればそれを使う
+    if (memo.has(n)) {
+        return memo.get(n); 
     }
-return fib(n - 1) + fib(n - 2);
-}
-const length = 40;
+    //メモにデータがないので計算する
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set (n, value);
+    return value;
+    }
+const length = 1000;
 for  (let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
